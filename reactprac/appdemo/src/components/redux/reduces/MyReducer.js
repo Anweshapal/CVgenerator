6e26ad080a58b289f0c9445783a27787b465a7bc
Anweshapal_ <86425625+Anweshapal@users.noreply.github.com>
2025-06-10 13:31:54 +0530

    Delete reactprac directory

--- reactprac/appdemo/src/components/redux/reduces/MyReducer.js
+++
@@ -1,7 +0,0 @@
-const initialdata={
-    name:"",
-};
-const myReducer=(state=initialdata,action)=>{
-  return state;
-};
-export default myReducer;
--- reactprac/appdemo/src/components/redux/store/Store.js
+++
@@ -1,6 +0,0 @@
-import { legacy_createStore as createStore } from "redux";
-import myReducer from "../reduces/MyReducer";
-
-let mystore = createStore(myReducer); // ✅ Correct
-export default mystore;
- 
